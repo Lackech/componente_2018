9c67da94315a621db55c2e871d470072c2d90442
Alejandro Chacon <alro.97@hotmail.com>
2018-08-06 14:53:53 -0600

    Con los tags

--- script.php
+++ script.php
@@ -25,15 +25,7 @@ class Com_CongresoInstallerScript
 	 */
 	public function __construct(JAdapterInstance $adapter) {}
 
-	/**
-	 * Called before any type of action
-	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function preflight($route, JAdapterInstance $adapter) {}
+
 
 	/**
 	 * Called after any type of action
@@ -43,7 +35,13 @@ class Com_CongresoInstallerScript
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function postflight($route, JAdapterInstance $adapter) {}
+	public function postflight($route, JAdapterInstance $adapter) {
+
+
+
+
+
+	}
 
 	/**
 	 * Called on installation
@@ -52,21 +50,56 @@ class Com_CongresoInstallerScript
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function install(JAdapterInstance $adapter) {}
+	public function install(JAdapterInstance $adapter) {
 
-	/**
-	 * Called on update
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function update(JAdapterInstance $adapter) {}
 
-	/**
-	 * Called on uninstallation
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 */
-	public function uninstall(JAdapterInstance $adapter) {}
+		// Initialize a new category
+		/** @type  JTableCategory  $category  */
+		$category = JTable::getInstance('Category');
+
+		// Check if the Uncategorised category exists before adding it
+		if (!$category->load(array('extension' => 'com_congreso', 'title' => 'Uncategorised')))
+		{
+			$category->extension = 'com_congreso';
+			$category->title = 'Uncategorised';
+			$category->description = '';
+			$category->published = 1;
+			$category->access = 1;
+			$category->params = '{"category_layout":"","image":""}';
+			$category->metadata = '{"author":"","robots":""}';
+			$category->metadesc = '';
+			$category->metakey = '';
+			$category->language = '*';
+			$category->checked_out_time = JFactory::getDbo()->getNullDate();
+			$category->version = 1;
+			$category->hits = 0;
+			$category->modified_user_id = 0;
+			$category->checked_out = 0;
+
+			// Set the location in the tree
+			$category->setLocation(1, 'last-child');
+
+			// Check to make sure our data is valid
+			if (!$category->check())
+			{
+				JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_CONGRESO_ERROR_INSTALL_CATEGORY', $category->getError()));
+
+				return;
+			}
+
+			// Now store the category
+			if (!$category->store(true))
+			{
+				JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_CONGRESO_ERROR_INSTALL_CATEGORY', $category->getError()));
+
+				return;
+			}
+
+			// Build the path for our category
+			$category->rebuildPath($category->id);
+		}
+
+	}
+
+
 }
